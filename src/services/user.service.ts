f30e2b2fe8655961ab2abae7579684f1a1c7d0e2
MamitianaAntonio <ratolojanaharyantonio1@gmail.com>
2025-09-19 16:25:42 +0000

    feat : add function to login an user and generate here token

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,5 +1,6 @@
 import bcrypt from "bcryptjs";
 import prisma from "../prismaClient";
+import { generateToken } from "../utils/jwt";
 
 // function to register a new user
 export async function registerUser(
@@ -12,4 +13,16 @@ export async function registerUser(
   return await prisma.UserAccount.create({
     data: { email, password: hashedPassword, name },
   });
+}
+
+// function to login
+export async function loginUser(email: string, password: string) {
+  const user = await prisma.UserAccount.findUnique({ where: { email } });
+  if (!user) throw new Error("User not found");
+
+  const isValid = await bcrypt.compare(password, user.password);
+  if (!isValid) throw new Error("Invalid password");
+  const token = generateToken(user.id);
+
+  return { token, user };
 }
