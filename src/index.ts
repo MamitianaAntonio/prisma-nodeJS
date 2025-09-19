ea3e336540c60bd44cc7c6d4d37649712923455d
MamitianaAntonio <ratolojanaharyantonio1@gmail.com>
2025-09-19 15:51:08 +0000

    feat : implement server using express in the project

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,12 @@
-import { PrismaClient } from "@prisma/client/extension";
+import express from "express";
+import cors from "cors";
 
-const prisma = new PrismaClient();
+const app = express();
+const PORT = process.env.PORT || 3000;
 
-export default prisma;
+app.use(express.json());
+app.use(cors());
+
+app.listen(PORT, () => {
+  console.log("The server was running", PORT);
+});
