45ed60bd0f43dd27a184090dc93710146d9a6fba
MamitianaAntonio <ratolojanaharyantonio1@gmail.com>
2025-09-25 14:47:24 +0000

    feat : implement user routes on index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,17 @@
 import express from "express";
 import cors from "cors";
+import dotenv from "dotenv";
+import userRoutes from "./routes/user.routes.ts";
+
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(cors());
+
+app.use("/api/users", userRoutes);
 
 app.listen(PORT, () => {
   console.log("The server was running", PORT);
